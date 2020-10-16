c557efb9c4d4693e8d8d1e9ee4c501bc6acd194e
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-16 18:22:28 +0700

    IF1-10119011-Latihan2-DeklarasiVariabel

---
+++ src/if1/pkg10119011/latihan2/deklarasivariable/IF110119011Latihan2DeklarasiVariable.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan2.deklarasivariable;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ * 
+ */
+public class IF110119011Latihan2DeklarasiVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta Uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai Variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Variabel nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI );
+        System.out.println("Isi Variabel Logika = " + nilaiLogika );
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter );
+    }
+    
+}
